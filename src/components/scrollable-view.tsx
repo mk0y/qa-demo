d756b9ae27bc54d9b6952a8279a4f288a034b18f
Marko Jakic <jakicmeister@gmail.com>
2024-03-04 21:06:44 +0100

    Home -> Docs

--- src/components/scrollable-view.tsx
+++ src/components/scrollable-view.tsx
@@ -52,7 +52,7 @@ const ScrollableView = () => {
                 className="flex items-center gap-3 rounded-lg bg-gray-100 px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50"
               >
                 <HomeIcon className="size-4" />
-                Home
+                Docs
               </Link>
               <Link
                 to={`/query`}
